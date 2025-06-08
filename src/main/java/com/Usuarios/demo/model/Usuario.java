714af5d2b122fdb2debf1cad85461d0013a6a40d
baruc sanchez <barucgs@gmail.com>
2025-06-07 22:56:08 -0600

    ADD model usuario

--- src/main/java/com/Usuarios/demo/model/Usuario.java
+++ src/main/java/com/Usuarios/demo/model/Usuario.java
@@ -1,4 +1,10 @@
 package com.Usuarios.demo.model;
 
+@Data // genera getters, setters, toString, equals, hashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+    private Long id;
+    private String nombre;
+    private String correo;
 }
