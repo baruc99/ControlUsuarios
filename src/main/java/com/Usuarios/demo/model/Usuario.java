16ee1af83561642d819ff1cfe6bd0811d6d6bc0d
baruc sanchez <barucgs@gmail.com>
2025-06-08 23:00:38 -0600

    FIX Usuario.java

--- src/main/java/com/Usuarios/demo/model/Usuario.java
+++ src/main/java/com/Usuarios/demo/model/Usuario.java
@@ -1,5 +1,10 @@
 package com.Usuarios.demo.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data // genera getters, setters, toString, equals, hashCode
 @NoArgsConstructor
 @AllArgsConstructor
