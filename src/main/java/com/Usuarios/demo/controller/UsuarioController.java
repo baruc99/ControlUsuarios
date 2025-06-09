6191d2f7e5a86fc25b9ebf02f0f8be7ba863e528
baruc sanchez <barucgs@gmail.com>
2025-06-08 23:10:02 -0600

    ADD UsuarioController.java

--- src/main/java/com/Usuarios/demo/controller/UsuarioController.java
+++ src/main/java/com/Usuarios/demo/controller/UsuarioController.java
@@ -1,4 +1,32 @@
 package com.Usuarios.demo.controller;
 
+
+import com.Usuarios.demo.model.Usuario;
+import com.Usuarios.demo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> obtenerUsuarios() {
+        return usuarioService.obtenerTodos();
+    }
+
+    @PostMapping
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.guardar(usuario);
+    }
+
+    @GetMapping("/buscar")
+    public Usuario buscarPorCorreo(@RequestParam String correo) {
+        return usuarioService.buscarPorCorreo(correo);
+    }
 }
